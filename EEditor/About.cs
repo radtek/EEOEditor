905d1c2b34749cdaf2362f120a2e25feb7c11417
capasha <capasha@protonmail.com>
2020-11-13 15:55:21 +0100

    First upload

---
+++ EEditor/About.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+using SharpCompress.Archives;
+using SharpCompress.Readers;
+using System.Diagnostics;
+namespace EEditor
+{
+    public partial class About : Form
+    {
+        MainForm Frm1;
+        public About(MainForm F)
+        {
+            InitializeComponent();
+            this.Text = "About EEditor " + this.ProductVersion;
+            Frm1 = F;
+        }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+            this.ForeColor = MainForm.themecolors.foreground;
+            this.BackColor = MainForm.themecolors.background;
+            for (int i = 0;i < this.Controls.Count;i++)
+            {
+                if (this.Controls[i].GetType() == typeof(GroupBox))
+                {
+                    this.Controls[i].ForeColor = MainForm.themecolors.foreground;
+                    for (int a = 0;a < this.Controls[i].Controls.Count;a++)
+                    {
+                        if (this.Controls[i].Controls[a].GetType() == typeof(Label))
+                        {
+                            this.Controls[i].Controls[a].ForeColor = MainForm.themecolors.foreground;
+                            this.Controls[i].Controls[a].BackColor = MainForm.themecolors.background;
+                        }
+                        if (this.Controls[i].Controls[a].GetType() == typeof(LinkLabel))
+                        {
+                            ((LinkLabel)this.Controls[i].Controls[a]).ForeColor = MainForm.themecolors.foreground;
+                            ((LinkLabel)this.Controls[i].Controls[a]).BackColor = MainForm.themecolors.background;
+                            ((LinkLabel)this.Controls[i].Controls[a]).LinkColor = MainForm.themecolors.link;
+                            ((LinkLabel)this.Controls[i].Controls[a]).VisitedLinkColor = MainForm.themecolors.visitedlink;
+                            ((LinkLabel)this.Controls[i].Controls[a]).ActiveLinkColor = MainForm.themecolors.activelink;
+                            
+
+
+                        }
+                        if (this.Controls[i].Controls[a].GetType() == typeof(Button))
+                        {
+                            ((Button)this.Controls[i].Controls[a]).ForeColor = MainForm.themecolors.foreground;
+                            ((Button)this.Controls[i].Controls[a]).BackColor = MainForm.themecolors.accent;
+                            ((Button)this.Controls[i].Controls[a]).FlatStyle = FlatStyle.Flat;
+                        }
+                    }
+                }
+                //if (this.Controls[i].name)
+            }
+    }
+
+        #region Main links
+        private void Button_Click(object sender, EventArgs e)
+        {
+            string link1 = null;
+            switch (((Button)sender).Name.ToString())
+            {
+                case "ForumButton":
+                        link1 = "http://forums.everybodyedits.com/viewtopic.php?id=32502";
+                    break;
+                case "BugsOrFeatureButton":
+                        link1 = "https://github.com/capasha/EEditor-/issues";
+                    break;
+                case "CreditButton":
+                        link1 = "https://github.com/capasha/EEditor-/wiki/Credits";
+                    break;
+                case "WikiButton":
+                        link1 = "https://github.com/capasha/EEditor-/wiki";
+                    break;
+                case "HomepageButton":
+                        link1 = "https://github.com/capasha/EEditor-";
+                    break;
+            }
+            DialogResult dgresult = MessageBox.Show($"Do you want to open {link1}\nin your webbrowser?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dgresult == DialogResult.Yes)
+            {
+                Process.Start(link1);
+            }
+        }
+        #endregion
+
+        #region Check version
+
+
+        #endregion
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string link = null;
+            switch (((LinkLabel)sender).Text)
+            {
+                case "font and text insertion":
+                    link = "https://forums.everybodyedits.com/viewtopic.php?pid=515488#p515488";
+                    break;
+                case "Google":
+                    link = "https://github.com/google/material-design-icons";
+                    break;
+                case "Material Design Icons contributors":
+                    link = "https://materialdesignicons.com/";
+                    break;
+                case "Fatcow Icons":
+                    link = "https://www.fatcow.com/free-icons";
+                    break;
+                case "Bresenham's Algorithm (Ellipse,Line)":
+                    link = "http://members.chello.at/~easyfilter/bresenham.html";
+                    break;
+                case "Json.NET Newtonsoft":
+                    link = "https://www.newtonsoft.com/json";
+                    break;
+                case "PlayerIO SDK":
+                    link = "https://playerio.com/download/";
+                    break;
+                case "Costura Fody - DLL Packer":
+                    link = "https://github.com/Fody/Costura";
+                    break;
+                case "SharpCompress - ZIP tool":
+                    link = "https://github.com/adamhathcock/sharpcompress";
+                    break;
+                case "Yonom/Processor's InitParser":
+                    link = "https://gist.github.com/Yonom/3c9ebfe69b1432452f9b";
+                    break;
+                case "Lukem's .eelvl parser":
+                    link = "https://gitlab.com/LukeM212/EELVL/tree/legacy";
+                    break;
+            }
+            DialogResult dr = MessageBox.Show($"Do you want to open {link}\nin your webbrowser?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dr == DialogResult.Yes)
+            {
+                Process.Start(link);
+            }
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            var file = $"{Directory.GetCurrentDirectory()}\\EEditorUpdater.exe";
+            if (File.Exists(file))
+            {
+                Process.Start(file);
+            }
+        }
+
+        private void linkLabel_MouseHover(object sender, EventArgs e)
+        {
+            ((LinkLabel)sender).LinkColor = MainForm.themecolors.activelink;
+        }
+
+        private void linkLabel_MouseLeave(object sender, EventArgs e)
+        {
+            ((LinkLabel)sender).LinkColor = MainForm.themecolors.link;
+        }
+    }
+}
---
+++ EEditor/SettingsForm.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace EEditor
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usePenToolCheckBox = new System.Windows.Forms.CheckBox();
+            this.selectAllBorderCheckBox = new System.Windows.Forms.CheckBox();
+            this.clearComboBox = new System.Windows.Forms.ComboBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.confirmCloseCheckBox = new System.Windows.Forms.CheckBox();
+            this.FasterShapeStyleCheckBox = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusTextToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusColorToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.UpdateCheckCheckBox = new System.Windows.Forms.CheckBox();
+            this.DarkThemeCheckBox = new System.Windows.Forms.CheckBox();
+            this.cBHotkeyBar = new System.Windows.Forms.CheckBox();
+            this.cBOldSel = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // usePenToolCheckBox
+            // 
+            this.usePenToolCheckBox.AutoSize = true;
+            this.usePenToolCheckBox.Location = new System.Drawing.Point(11, 15);
+            this.usePenToolCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.usePenToolCheckBox.Name = "usePenToolCheckBox";
+            this.usePenToolCheckBox.Size = new System.Drawing.Size(184, 17);
+            this.usePenToolCheckBox.TabIndex = 8;
+            this.usePenToolCheckBox.Text = "Select Pen tool after block switch";
+            this.usePenToolCheckBox.UseVisualStyleBackColor = true;
+            this.usePenToolCheckBox.CheckedChanged += new System.EventHandler(this.usePenToolCheckBox_CheckedChanged);
+            // 
+            // selectAllBorderCheckBox
+            // 
+            this.selectAllBorderCheckBox.AutoSize = true;
+            this.selectAllBorderCheckBox.Location = new System.Drawing.Point(11, 36);
+            this.selectAllBorderCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.selectAllBorderCheckBox.Name = "selectAllBorderCheckBox";
+            this.selectAllBorderCheckBox.Size = new System.Drawing.Size(165, 17);
+            this.selectAllBorderCheckBox.TabIndex = 18;
+            this.selectAllBorderCheckBox.Text = "Include borders with select all";
+            this.selectAllBorderCheckBox.UseVisualStyleBackColor = true;
+            this.selectAllBorderCheckBox.CheckedChanged += new System.EventHandler(this.selectAllBorderCheckBox_CheckedChanged);
+            // 
+            // clearComboBox
+            // 
+            this.clearComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.clearComboBox.Items.AddRange(new object[] {
+            "Clear settings...",
+            "",
+            "Block hotkeys (0-9)",
+            "Blocks in unknown tab",
+            "",
+            "Old EEditor settings & logins",
+            "Current EEditor settings"});
+            this.clearComboBox.Location = new System.Drawing.Point(11, 208);
+            this.clearComboBox.Name = "clearComboBox";
+            this.clearComboBox.Size = new System.Drawing.Size(139, 21);
+            this.clearComboBox.TabIndex = 27;
+            this.clearComboBox.SelectedIndexChanged += new System.EventHandler(this.clearComboBox_SelectedIndexChanged);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clearButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.clearButton.Location = new System.Drawing.Point(156, 206);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(49, 23);
+            this.clearButton.TabIndex = 28;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // confirmCloseCheckBox
+            // 
+            this.confirmCloseCheckBox.AutoSize = true;
+            this.confirmCloseCheckBox.Location = new System.Drawing.Point(11, 57);
+            this.confirmCloseCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.confirmCloseCheckBox.Name = "confirmCloseCheckBox";
+            this.confirmCloseCheckBox.Size = new System.Drawing.Size(117, 17);
+            this.confirmCloseCheckBox.TabIndex = 29;
+            this.confirmCloseCheckBox.Text = "Confirm EEditor exit";
+            this.confirmCloseCheckBox.UseVisualStyleBackColor = true;
+            this.confirmCloseCheckBox.CheckedChanged += new System.EventHandler(this.confirmCloseCheckBox_CheckedChanged);
+            // 
+            // FasterShapeStyleCheckBox
+            // 
+            this.FasterShapeStyleCheckBox.AutoSize = true;
+            this.FasterShapeStyleCheckBox.Enabled = false;
+            this.FasterShapeStyleCheckBox.Location = new System.Drawing.Point(11, 185);
+            this.FasterShapeStyleCheckBox.Name = "FasterShapeStyleCheckBox";
+            this.FasterShapeStyleCheckBox.Size = new System.Drawing.Size(111, 17);
+            this.FasterShapeStyleCheckBox.TabIndex = 30;
+            this.FasterShapeStyleCheckBox.Text = "Faster shape style";
+            this.FasterShapeStyleCheckBox.UseVisualStyleBackColor = true;
+            this.FasterShapeStyleCheckBox.Visible = false;
+            this.FasterShapeStyleCheckBox.CheckedChanged += new System.EventHandler(this.FasterShapeStyleCheckBox_CheckedChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusTextToolStripStatusLabel,
+            this.StatusToolStripStatusLabel,
+            this.StatusColorToolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 246);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(231, 22);
+            this.statusStrip1.TabIndex = 31;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusTextToolStripStatusLabel
+            // 
+            this.StatusTextToolStripStatusLabel.Name = "StatusTextToolStripStatusLabel";
+            this.StatusTextToolStripStatusLabel.Size = new System.Drawing.Size(42, 17);
+            this.StatusTextToolStripStatusLabel.Text = "Status:";
+            this.StatusTextToolStripStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // StatusToolStripStatusLabel
+            // 
+            this.StatusToolStripStatusLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.StatusToolStripStatusLabel.Name = "StatusToolStripStatusLabel";
+            this.StatusToolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // StatusColorToolStripStatusLabel
+            // 
+            this.StatusColorToolStripStatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.StatusColorToolStripStatusLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.StatusColorToolStripStatusLabel.Name = "StatusColorToolStripStatusLabel";
+            this.StatusColorToolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // UpdateCheckCheckBox
+            // 
+            this.UpdateCheckCheckBox.AutoSize = true;
+            this.UpdateCheckCheckBox.Checked = true;
+            this.UpdateCheckCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.UpdateCheckCheckBox.Location = new System.Drawing.Point(11, 79);
+            this.UpdateCheckCheckBox.Name = "UpdateCheckCheckBox";
+            this.UpdateCheckCheckBox.Size = new System.Drawing.Size(113, 17);
+            this.UpdateCheckCheckBox.TabIndex = 32;
+            this.UpdateCheckCheckBox.Text = "Check for updates";
+            this.UpdateCheckCheckBox.UseVisualStyleBackColor = true;
+            this.UpdateCheckCheckBox.CheckedChanged += new System.EventHandler(this.UpdateCheckCheckBox_CheckedChanged);
+            // 
+            // DarkThemeCheckBox
+            // 
+            this.DarkThemeCheckBox.AutoSize = true;
+            this.DarkThemeCheckBox.Location = new System.Drawing.Point(11, 102);
+            this.DarkThemeCheckBox.Name = "DarkThemeCheckBox";
+            this.DarkThemeCheckBox.Size = new System.Drawing.Size(85, 17);
+            this.DarkThemeCheckBox.TabIndex = 34;
+            this.DarkThemeCheckBox.Text = "Dark Theme";
+            this.DarkThemeCheckBox.UseVisualStyleBackColor = true;
+            this.DarkThemeCheckBox.CheckedChanged += new System.EventHandler(this.DarkThemeCheckBox_CheckedChanged);
+            // 
+            // cBHotkeyBar
+            // 
+            this.cBHotkeyBar.AutoSize = true;
+            this.cBHotkeyBar.Location = new System.Drawing.Point(11, 125);
+            this.cBHotkeyBar.Name = "cBHotkeyBar";
+            this.cBHotkeyBar.Size = new System.Drawing.Size(106, 17);
+            this.cBHotkeyBar.TabIndex = 35;
+            this.cBHotkeyBar.Text = "Show HotkeyBar";
+            this.cBHotkeyBar.UseVisualStyleBackColor = true;
+            this.cBHotkeyBar.CheckedChanged += new System.EventHandler(this.cBHotkeyBar_CheckedChanged);
+            // 
+            // cBOldSel
+            // 
+            this.cBOldSel.AutoSize = true;
+            this.cBOldSel.Checked = true;
+            this.cBOldSel.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cBOldSel.Location = new System.Drawing.Point(11, 148);
+            this.cBOldSel.Name = "cBOldSel";
+            this.cBOldSel.Size = new System.Drawing.Size(120, 17);
+            this.cBOldSel.TabIndex = 36;
+            this.cBOldSel.Text = "New Selection Way";
+            this.cBOldSel.UseVisualStyleBackColor = true;
+            this.cBOldSel.CheckedChanged += new System.EventHandler(this.cBOldSel_CheckedChanged);
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 268);
+            this.Controls.Add(this.cBOldSel);
+            this.Controls.Add(this.cBHotkeyBar);
+            this.Controls.Add(this.DarkThemeCheckBox);
+            this.Controls.Add(this.UpdateCheckCheckBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.FasterShapeStyleCheckBox);
+            this.Controls.Add(this.usePenToolCheckBox);
+            this.Controls.Add(this.selectAllBorderCheckBox);
+            this.Controls.Add(this.confirmCloseCheckBox);
+            this.Controls.Add(this.clearComboBox);
+            this.Controls.Add(this.clearButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "SettingsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EEditor Settings";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SettingsForm_FormClosing);
+            this.Load += new System.EventHandler(this.SettingsForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox usePenToolCheckBox;
+        private System.Windows.Forms.CheckBox selectAllBorderCheckBox;
+        private System.Windows.Forms.ComboBox clearComboBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.CheckBox confirmCloseCheckBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusTextToolStripStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel StatusToolStripStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel StatusColorToolStripStatusLabel;
+        private System.Windows.Forms.CheckBox FasterShapeStyleCheckBox;
+        private System.Windows.Forms.CheckBox UpdateCheckCheckBox;
+        private System.Windows.Forms.CheckBox DarkThemeCheckBox;
+        private System.Windows.Forms.CheckBox cBHotkeyBar;
+        private System.Windows.Forms.CheckBox cBOldSel;
+    }
+}
