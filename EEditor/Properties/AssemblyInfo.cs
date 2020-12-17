37576f3fe641f78d348862b857eca2463deea049
capasha <capasha@protonmail.com>
2020-12-17 14:21:20 +0100

    EEOditor new version 1.0.0

--- EEditor/Properties/AssemblyInfo.cs
+++ EEditor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EEditor")]
-[assembly: AssemblyDescription("EEditor is a tool made to simplify Everybody Edits world editing and creation.")]
+[assembly: AssemblyTitle("EEOditor")]
+[assembly: AssemblyDescription("EEOditor is a tool made to simplify Everybody Edits Offline world editing and creation.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EEditor Authors")]
-[assembly: AssemblyProduct("EEditor")]
+[assembly: AssemblyProduct("EEOditor")]
 [assembly: AssemblyCopyright("Copyright © EEditor Authors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
