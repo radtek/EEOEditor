18ccce257ad01b9ddbb4ee2d3f73024364e85878
capasha <capasha@protonmail.com>
2021-10-25 22:42:39 +0200

    updated designer

--- EEditor/NewDialogForm.Designer.cs
+++ EEditor/NewDialogForm.Designer.cs
@@ -38,10 +38,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.ltitle = new System.Windows.Forms.Label();
-            this.lname = new System.Windows.Forms.Label();
-            this.tbtitle = new System.Windows.Forms.TextBox();
             this.tbowner = new System.Windows.Forms.TextBox();
+            this.tbtitle = new System.Windows.Forms.TextBox();
+            this.lname = new System.Windows.Forms.Label();
+            this.ltitle = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nUHeight)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nUWidth)).BeginInit();
@@ -57,7 +57,7 @@
             this.groupBox1.Controls.Add(this.lWidth);
             this.groupBox1.Location = new System.Drawing.Point(12, 128);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(168, 88);
+            this.groupBox1.Size = new System.Drawing.Size(168, 85);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Size";
@@ -65,8 +65,13 @@
             // nUHeight
             // 
             this.nUHeight.Location = new System.Drawing.Point(56, 49);
+            this.nUHeight.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
             this.nUHeight.Minimum = new decimal(new int[] {
-            25,
+            3,
             0,
             0,
             0});
@@ -91,8 +96,13 @@
             // nUWidth
             // 
             this.nUWidth.Location = new System.Drawing.Point(56, 25);
+            this.nUWidth.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
             this.nUWidth.Minimum = new decimal(new int[] {
-            25,
+            3,
             0,
             0,
             0});
@@ -116,9 +126,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 232);
+            this.button1.Location = new System.Drawing.Point(48, 219);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 23);
+            this.button1.Size = new System.Drawing.Size(86, 26);
             this.button1.TabIndex = 7;
             this.button1.Text = "Create";
             this.button1.UseVisualStyleBackColor = true;
@@ -132,17 +142,19 @@
             this.button2.TabIndex = 21;
             this.button2.Text = "Background color";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.button2);
-            this.groupBox4.Location = new System.Drawing.Point(186, 128);
+            this.groupBox4.Location = new System.Drawing.Point(288, 12);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(96, 64);
             this.groupBox4.TabIndex = 31;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Properties";
+            this.groupBox4.Visible = false;
             // 
             // groupBox2
             // 
@@ -152,19 +164,26 @@
             this.groupBox2.Controls.Add(this.ltitle);
             this.groupBox2.Location = new System.Drawing.Point(12, 8);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(270, 114);
+            this.groupBox2.Size = new System.Drawing.Size(218, 114);
             this.groupBox2.TabIndex = 32;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Information";
             // 
-            // ltitle
+            // tbowner
             // 
-            this.ltitle.AutoSize = true;
-            this.ltitle.Location = new System.Drawing.Point(12, 25);
-            this.ltitle.Name = "ltitle";
-            this.ltitle.Size = new System.Drawing.Size(30, 13);
-            this.ltitle.TabIndex = 0;
-            this.ltitle.Text = "Title:";
+            this.tbowner.Location = new System.Drawing.Point(15, 81);
+            this.tbowner.Name = "tbowner";
+            this.tbowner.Size = new System.Drawing.Size(139, 20);
+            this.tbowner.TabIndex = 3;
+            this.tbowner.Text = "Player";
+            // 
+            // tbtitle
+            // 
+            this.tbtitle.Location = new System.Drawing.Point(15, 41);
+            this.tbtitle.Name = "tbtitle";
+            this.tbtitle.Size = new System.Drawing.Size(190, 20);
+            this.tbtitle.TabIndex = 2;
+            this.tbtitle.Text = "Untitled World";
             // 
             // lname
             // 
@@ -175,37 +194,30 @@
             this.lname.TabIndex = 1;
             this.lname.Text = "Owner:";
             // 
-            // tbtitle
+            // ltitle
             // 
-            this.tbtitle.Location = new System.Drawing.Point(15, 41);
-            this.tbtitle.Name = "tbtitle";
-            this.tbtitle.Size = new System.Drawing.Size(190, 20);
-            this.tbtitle.TabIndex = 2;
-            this.tbtitle.Text = "Untitled World";
-            // 
-            // tbowner
-            // 
-            this.tbowner.Location = new System.Drawing.Point(15, 81);
-            this.tbowner.Name = "tbowner";
-            this.tbowner.Size = new System.Drawing.Size(139, 20);
-            this.tbowner.TabIndex = 3;
-            this.tbowner.Text = "Player";
+            this.ltitle.AutoSize = true;
+            this.ltitle.Location = new System.Drawing.Point(12, 25);
+            this.ltitle.Name = "ltitle";
+            this.ltitle.Size = new System.Drawing.Size(30, 13);
+            this.ltitle.TabIndex = 0;
+            this.ltitle.Text = "Title:";
             // 
             // NewDialogForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 268);
+            this.ClientSize = new System.Drawing.Size(243, 255);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "NewDialogForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "New";
+            this.Text = "New World";
             this.Load += new System.EventHandler(this.NewDialogForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
